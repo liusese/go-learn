ddd562b56909486c1e28986973c117b005b8012e
liusese <shijing.liu@qq.com>
2021-04-27 16:46:34 +0800

    week3

--- go.mod
+++ go.mod
@@ -7,4 +7,5 @@ require (
 	github.com/pkg/errors v0.9.1
 	github.com/pquerna/ffjson v0.0.0-20190930134022-aa0246cd15f7
 	github.com/smartystreets/goconvey v1.6.4
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c
 )
---
+++ week3/homework/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"golang.org/x/sync/errgroup"
+)
+
+func main() {
+	logger := log.New(os.Stdout, "my app: ", log.LstdFlags)
+
+	server := runServer(logger)
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, syscall.SIGHUP, syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGINT)
+
+	eg, ctx := errgroup.WithContext(context.Background())
+	eg.Go(func() (err error) {
+		for {
+			select {
+			case <-ctx.Done():
+				err = ctx.Err()
+				return
+			case <-c:
+				server.SetKeepAlivesEnabled(false)
+				err = server.Shutdown(ctx)
+				return
+			}
+		}
+	})
+
+	_ = server.ListenAndServe()
+
+	if err := eg.Wait(); err != nil && !errors.Is(err, context.Canceled) {
+		logger.Fatalf("shutdown server found error: %v\n", err)
+	}
+
+	logger.Println("server gracefully shutdown")
+}
+
+func runServer(logger *log.Logger) *http.Server {
+	router := http.NewServeMux()
+	router.HandleFunc("/show", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`OK`))
+	})
+
+	return &http.Server{
+		Addr:         ":8080",
+		Handler:      router,
+		ErrorLog:     logger,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  15 * time.Second,
+	}
+}
